477971a8ff980e4f32aca67aa71f0e4c36ed6949
ashuvssut <ashu.khanduala@gmail.com>
2020-07-23 23:41:46 +0530

    event.html first release upload

---
+++ public/js/button-click.js
@@ -0,0 +1,7 @@
+const pullerButtons = document.querySelectorAll('.puller-btn');
+
+pullerButtons.forEach((pullerButton, index) => {
+    pullerButton.addEventListener('click', (event)=>{
+        document.querySelector(`.drop-list-${index}`).classList.toggle('close');
+    })
+});
